34224716ae241d73db34a3218d454c2bee4ede1a
Dan <dpedersen84@gmail.com>
2018-03-02 23:08:07 -0600

    added functions, wont print to screen yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,35 @@ var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
                 "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
                 "u", "v", "w", "x", "y", "z"];
 
-var turns = 10;
+var turns = 9;
 
 var wins = 0;
 
 var losses = 0;
 
-// Game Picks Random Letter
+var letterPicked = [];
+
+// Game Picks Random Letter(sets letterRandom variable to random choice of letter array)
 
 var letterRandom = letter[Math.floor(Math.random() * letter.length)];
 console.log(letterRandom);
 
-// Capture key
+// Sets turnsRemaining Function to show Turns Left on main page
+
+function turnsRemaining () {
+    document.querySelector("#turns").innerHTML = "Turns Left: " + turns;
+    console.log(turnsRemaining);
+}
+
+
+
+// turnsRemaining (); 
+
+// Sets l
+
+
+
+// // Capture key
 
 document.onkeyup = function(event) {
     console.log(event.key);
@@ -26,12 +43,12 @@ document.onkeyup = function(event) {
     if (letterRandom === event.key) {
     alert("Correct!") //Correct Alert Window
     wins = wins + 1;
-    console.log(wins);
+    console.log("Wins = "  + wins);
     document.querySelector("#wins").innerHTML = "Wins: " + wins; 
     } else {
     alert("wrong!") //Wrong Alert Window
     losses = losses + 1;
-    console.log(losses);
+    console.log("Losses = " + losses);
     document.querySelector("#losses").innerHTML = "Losses: " + losses;
     }
 
