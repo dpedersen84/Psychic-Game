a44082bd37dd97196802d7036c481aa6eaf0bb6d
Dan <dpedersen84@gmail.com>
2018-03-03 22:31:59 -0600

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,124 @@
-// Arrays
-
-const letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+// Letters To Choose
+var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
                 "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
                 "u", "v", "w", "x", "y", "z"];
 
-let turns = 9;
+//Stats Default
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesLeft = 10;
+var guessedLetters = [];
+var guessThisLetter = null;
 
-let wins = 0;
+//Computer Picks Letter
+var computerChoice = computerLetters[Math.floor(Math.random() * computerLetters.length)];
 
-let losses = 0;
+//Guesses Are Set To 10
 
-// let letterPicked = [];
+function newGuessesLeft () {
+    //Print to DOM
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+};
 
-// Game Picks Random Letter(sets letterRandom variable to random choice of letter array)
-
-let letterRandom = letter[Math.floor(Math.random() * letter.length)];
-console.log("Computer pick = " + letterRandom);
-
-// Sets turnsRemaining Function to show Turns Left on main page
-
-function turnsRemaining () {
-    document.getElementById("turns").innerHTML = "Turns Left: " + turns;
-    
-}
-
-// Sets lettersUsed function to show Letters picked on main page
-
-function lettersUsed () {
-    document.getElementById("letterPicked").innerHTML = "Letters Picked: " + letterPicked.join(' ');
-    
-}
-
-// Reset function
-
-function reset () {
-    var turns = 9;
-
-}
-
-// // Capture key
-
-document.onkeyup = function(event) {
-    console.log("Key Pressed = " + event.key); // Key user presses
-
-    var letterPicked = [];
-
-    document.getElementById("letters").innerHTML = letterPicked;
-    
-    function lettersUsed () {
-        letterPicked.push(event.key);
-        document.getElementById("letters").innerHTML = letterPicked;
-    }
-
-    // Determines whether key pressed is correct guess
-    if (letterRandom === event.key) {
-    alert("Correct!") //Correct Alert Window
-    wins = wins + 1;
-    console.log("Wins = "  + wins);
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-    reset ();
-    
-    } else {
-    alert("wrong!") //Wrong Alert Window
-    losses = losses + 1;
-    console.log("Losses = " + losses);
-    document.getElementById("losses").innerHTML = "Losses: " + losses;
-    reset ();
-    }
-
+//Computer Picks Letter
+function newGuessThisLetter () {
+    this.guessThisLetter = this.computerLetters[Math.floor(Math.random() * this.computerLetters.length)];
 
 };
+
+//What Has Been Guessed
+function newGuessedLetters () {
+    //Print to DOM
+    document.getElementById("letters").innerHTML = "Letters Guessed: " + guessedLetters.join(", ");
+};
+
+//Reset Function
+function reset () {
+    guessesLeft = 10;
+    guessedLetters = [];
+
+    //Call
+    newGuessesLeft();
+    newGuessThisLetter();
+    newGuessedLetters();
+}
+//Call
+newGuessThisLetter();
+newGuessesLeft();
+
+//Key Event 
+document.onkeyup = function(event) {
+    guessesLeft--;
+    console.log(guessesLeft);
+    console.log(event.key);
+    //Save Key Even To Variable
+    var letter = String.fromCharCode(event.which).toLowerCase();
+    //Put Letter Variable into Guessed Letters Array
+    guessedLetters.push(letter);
+
+
+
+    if (letter === guessThisLetter) {
+        alert("Win!");
+        reset();
+    } else {
+        alert("Wrong!");
+    }
+};
+
+// // Game Picks Random Letter(sets letterRandom variable to random choice of letter array)
+
+// randomLetter = computerLetters[Math.floor(Math.random() * computerLetters.length)];
+// console.log("Computer pick = " + randomLetter);
+
+
+// // // userTurns to HTML
+// function newUserTurns () {
+// document.getElementById("#userTurnsLeft").innerHTML =  "Turns Left: " + userTurns;
+// };
+
+// var newComputerChoice = function () {
+//     this.computerChoice = this.computerLetters[Math.floor(Math.random() * this.computerLetters.length)];
+// };
+
+// var userHasGuessed = function () {
+//     document.getElementById("#userGuess").innerHTML = "Guessed: " + userGuesses.join(", ");
+// };
+
+// var reset = function () {
+//     userTurns = 9;
+//     userGuesses = [];
+
+//     newUserTurns ();
+//     newComputerChoice ();
+//     userHasGuessed ();
+// }
+
+
+// // // Capture key
+// document.onkeyup = function(event) {
+//     console.log("Key Pressed = " + event.key); // Key user presses
+
+//     var x = String.fromCharCode(event.key).toLowerCase();
+//     document.getElementById("guess").innerHTML = x;
+// }
+
+//     // // Determines whether key pressed is correct guess
+//     // if (randomLetter === event.key) {
+//     // alert("Correct!") //Correct Alert Window
+//     // userWins ++; // Update Wins
+//     // console.log("Wins = "  + userWins); // Console Log Wins
+//     // document.getElementById("wins").innerHTML = "Wins: " + userWins; // Update Wins on DOM
+
+    
+//     // } else {
+//     // alert("Wrong!") //Wrong Alert Window
+//     // userTurns --; // Reduce Turns
+//     // document.getElementById("userGuess").innherHTML = "Guessed:" + letterPicked;
+//     // console.log("Turns = " + userTurns); // Console Log Losses
+//     // document.getElementById("userTurnsLeft").innerHTML = "Turns Left: " + userTurns; // Update Turns on DOM
+    
+//     // };
+
 
